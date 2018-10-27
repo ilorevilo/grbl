9cde08e0c33c2a1acddd8a2d657f4fb80721e8ad
ilorevilo <44391679+ilorevilo@users.noreply.github.com>
2018-10-28 00:26:50 +0200

    Update cpu_map.h
    - change step <-> direction pins
    - set only one limit Pin: D10

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -36,17 +36,17 @@
   // Define step pulse output pins. NOTE: All step bit pins must be on the same port.
   #define STEP_DDR        DDRD
   #define STEP_PORT       PORTD
-  #define X_STEP_BIT      2  // Uno Digital Pin 2
-  #define Y_STEP_BIT      3  // Uno Digital Pin 3
-  #define Z_STEP_BIT      4  // Uno Digital Pin 4
+  #define X_STEP_BIT      5  // Uno Digital Pin 2 --> switch with direction
+  #define Y_STEP_BIT      6  // Uno Digital Pin 3 --> switch with direction
+  #define Z_STEP_BIT      7  // Uno Digital Pin 4 --> switch with direction
   #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
 
   // Define step direction output pins. NOTE: All direction pins must be on the same port.
   #define DIRECTION_DDR     DDRD
   #define DIRECTION_PORT    PORTD
-  #define X_DIRECTION_BIT   5  // Uno Digital Pin 5
-  #define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
-  #define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
+  #define X_DIRECTION_BIT   2  // Uno Digital Pin 5 --> switch with step
+  #define Y_DIRECTION_BIT   3  // Uno Digital Pin 6 --> switch with step
+  #define Z_DIRECTION_BIT   4  // Uno Digital Pin 7 --> switch with step
   #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
 
   // Define stepper driver enable/disable output pin.
@@ -60,14 +60,22 @@
   #define LIMIT_DDR        DDRB
   #define LIMIT_PIN        PINB
   #define LIMIT_PORT       PORTB
-  #define X_LIMIT_BIT      1  // Uno Digital Pin 9
-  #define Y_LIMIT_BIT      2  // Uno Digital Pin 10
+  
+  //#define X_LIMIT_BIT      1  // Uno Digital Pin 9
+  //#define Y_LIMIT_BIT      2  // Uno Digital Pin 10
+
+  #define LIMIT_BIT   2 // set only one limit pin: Uno Digital Pin 10
+
+/*
   #ifdef VARIABLE_SPINDLE // Z Limit pin and spindle enabled swapped to access hardware PWM on Pin 11.
     #define Z_LIMIT_BIT	   4 // Uno Digital Pin 12
   #else
     #define Z_LIMIT_BIT    3  // Uno Digital Pin 11
   #endif
-  #define LIMIT_MASK       ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
+  */
+  //#define LIMIT_MASK       ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
+  #define LIMIT_MASK (1<<LIMIT_BIT)
+  
   #define LIMIT_INT        PCIE0  // Pin change interrupt enable pin
   #define LIMIT_INT_vect   PCINT0_vect
   #define LIMIT_PCMSK      PCMSK0 // Pin change interrupt register
